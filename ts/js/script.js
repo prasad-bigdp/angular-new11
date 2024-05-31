55afc0c2bf44d45be1711c4e7cae7200ad42ab15
prasad-bigdp <bigdp585@gmail.com>
2024-05-31 12:38:08 +0530

    ts2

--- ts/js/script.js
+++ ts/js/script.js
@@ -1,42 +1,60 @@
 "use strict";
-let a = "raj"; // assertion
-let b = 25; //inference
-console.log(typeof a, typeof b);
-console.log("hi");
-//
-let x = 5;
-//number - any numerical
-let str = `prasad`;
-let y = true;
-console.log(typeof y);
-let z = undefined;
-let x1 = null;
-let srn = "hdgh";
-srn = 25;
-let s = 25;
-let myarr = [5, 6, 25];
-// i have to store [26,"raj"]
-let myarr2 = [26, "raj"];
-//tuple
-let myarr3 = [26, "raj"];
-// functions will two values to give data types- arguments, return type
-function sum(a, b) {
-    console.log(a + b);
+let a = 25;
+console.log(typeof a);
+let b = 55;
+let c = "prasad";
+let d = true;
+let e = undefined;
+let f = null;
+let g = "raj";
+let h = [2, 5, 8, 7, 9];
+let i = ["raj", 25];
+let j = "dhgjhg";
+let k = [];
+let fun = (a, b) => a + b;
+function fun2(str) {
+    console.log(str);
 }
-sum(5, 6);
-// write a basic sum function which adds two strings and returns the result
-function sum2(a, b) {
-    return a + b;
+fun2("raj");
+function fun3() {
+    throw new Error("my error");
 }
-sum2("Hello", "world");
-const sum3 = (a, b) => a + b;
-try {
-    function bomb() {
-        throw "some exception";
+let l = [2, "hgh", true, 52];
+let obj = {
+    name: "raj",
+    age: 55885,
+    fn: (a) => a++,
+};
+let m = true;
+let n = "fjmfbvmfb";
+let o = 88;
+let myDetails = {
+    username: "something",
+    password: "something",
+    email: "a@a.com"
+};
+const sum = (a, b) => a + b;
+const sub = (a, b) => a - b;
+const mul = (a, b) => a * b;
+// Enums are used to specify some constants
+var days;
+(function (days) {
+    days[days["sunday"] = 0] = "sunday";
+    days[days["monday"] = 1] = "monday";
+    days[days["tuesday"] = 2] = "tuesday";
+    days[days["thursday"] = 3] = "thursday";
+    days[days["friday"] = 4] = "friday";
+    days[days["saturday"] = 5] = "saturday";
+})(days || (days = {}));
+console.log(days.sunday, days[0]); //0
+const inp = document.getElementById('inp');
+const x = inp;
+console.log(x.value);
+class A {
+    constructor(n, s, i) {
+        this.name = n,
+            this.salary = s,
+            this.id = i;
     }
-    bomb();
 }
-catch (err) {
-    console.log(err);
-}
-// type alias,inference,OOP
+let p = new A("raj", 18000, 25);
--- ts/script.ts
+++ ts/script.ts
@@ -1,41 +1,131 @@
-let a: string = "raj" // assertion
-let b = 25 //inference
-console.log(typeof a, typeof b)
-console.log("hi")
-//
-let x: number = 5
-//number - any numerical
-let str: string = `prasad`
-let y: boolean = true
-console.log(typeof y)
-let z: undefined = undefined
-let x1: null = null
-let srn: number | string = "hdgh"
-srn = 25
-let s: any = 25
-let myarr: number[] = [5, 6, 25]
-// i have to store [26,"raj"]
-let myarr2: (number | string)[] = [26, "raj"]
-//tuple
-let myarr3: [number, string] = [26, "raj"]
-// functions will two values to give data types- arguments, return type
+let a = 25;
+console.log(typeof a)
+let b: number = 55;
+let c: string = "prasad"
+let d: boolean = true
+let e: undefined = undefined;
+let f: null = null;
+let g: number | string = "raj"
+let h: number[] = [ 2, 5, 8, 7, 9 ]
+let i: (number | string)[] = [ "raj", 25 ]
+let j: any = "dhgjhg"
+let k: any[] = [];
 
-function sum(a: number, b: number): void {
-	console.log(a + b)
+let fun = (a: number, b: number): number => a + b
+function fun2(str:string):void
+{
+    console.log(str)
 }
-sum(5, 6)
-// write a basic sum function which adds two strings and returns the result
-function sum2(a: string, b: string): string {
-	return a + b
+fun2("raj")
+
+function fun3():never
+{
+    throw new Error("my error")
 }
-sum2("Hello", "world")
-const sum3 = (a: string, b: string): string => a + b
-try {
-	function bomb():never {
-		throw "some exception"
-	}
-	bomb()
-} catch (err) {
-	console.log(err)
+
+let l: [ number, string, boolean, number ] = [ 2, "hgh", true, 52 ]
+//tuple
+
+//interfaces
+interface myObj{
+    name: string,
+    age: number,
+    fn:(a:number)=>number
 }
-// type alias,inference,OOP
+
+let obj:myObj = {
+    name: "raj",
+    age: 55885,
+    fn: (a)=> a++,
+}
+
+// try to create an interface which accepts a basic registrration form which contains username,email,phno,pwd
+interface regForm {
+	username: string
+	phno?: number
+	email: string,
+	password: string
+}
+type nsb = number | string | boolean //type aliases
+let m: nsb = true
+let n: nsb = "fjmfbvmfb"
+let o: nsb = 88
+
+type regForm2 = {
+	username: string
+	phno?: number
+	email: string
+	password: string
+}
+let myDetails:regForm2 = {
+    username: "something",
+    password: "something",
+    email: "a@a.com"
+}
+
+const sum:myFun=(a, b)=>a+b
+const sub:myFun=(a,b)=>a-b
+const mul:myFun=(a,b)=>a*b
+type myFun= (a:number,b:number)=>number
+
+
+// Enums are used to specify some constants
+enum days{
+    sunday,
+    monday,
+    tuesday,
+    thursday,
+    friday,
+    saturday
+}
+console.log(days.sunday, days[ 0 ]) //0
+const inp = document.getElementById('inp')
+const x= inp as HTMLInputElement
+console.log(x.value)
+
+// OOP
+interface forA{
+    name: string,
+    salary: number,
+    id:number
+}
+class A implements forA {
+    name;
+    salary;
+    id;
+    constructor(n:string,s:number,i:number)
+    {
+        this.name=n,
+         this.salary=s,
+         this.id=i
+    }
+}
+let p= new A("raj",18000,25)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
